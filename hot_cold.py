b753ff18de17a0062d66db870e103de45c572a90
Wojciech Bogdan <w.bogdan@ymail.com>
2022-06-04 22:21:46 +0200

    Fix all pull requests apart from asking to play again

--- hot_cold.py
+++ hot_cold.py
@@ -1,30 +1,28 @@
 import random
 
 gues_num = random.randint(1, 51)
-print(gues_num)
-still_playing = True
 print('Guess a number btw 1 and 50 or enter -1 to quit')
 counter = 0
-while still_playing:
+while True:
     counter += 1
     try:
-        temp = int(input('Enter the number: '))
+        guessed_number = int(input('Enter the number: '))
     except ValueError as e:
         print('I said a number and you entered', e)
         continue
-    if temp == -1:
+    if guessed_number == -1:
         print("Quitting game on user's request...")
-        still_playing = False
-    if temp == gues_num:
-        print(f'You won in {counter} attempts')
         break
-    if temp > gues_num and temp <= 50:  # coz break above quits the program
+    if guessed_number == gues_num:
+        print(f'You won in {counter} atguessed_numberts')
+        break
+    if guessed_number > gues_num and guessed_number <= 50:  # coz break above quits the program
         print('too much')
         continue
-    if temp < gues_num and temp > 0:
+    if guessed_number < gues_num and guessed_number > 0:
         print('too little')
         continue
-    if temp == 0 or temp < -1 or temp > 50:
+    if guessed_number == 0 or guessed_number < -1 or guessed_number > 50:
         print('Between 1 and 50 I said...')
         continue
 
